6e7e86a7a4ca92d8818635d8093fc05eb38f6bbb
Thomas Terp <thomascterp@gmail.com>
2014-01-20 13:36:44 +0100

    Replaced tabs with spaces

--- useful_lua_library/lua/includes/ull.lua
+++ useful_lua_library/lua/includes/ull.lua
@@ -13,16 +13,16 @@ ULL = ULL or {}
 
 --Info--
 ULL.INFO = {
-	name 	   = "Useful Lua Library",
-	hypocorism = "ULL",
-	version    = 1.0,
-	
-	author = {
-		name 	= "Thomas",
-		country = "Denmark",
-		profile = "http://steamcommunity.com/profiles/76561197999017482/",
-		steamID = "STEAM_0:0:19375877",
-	},
+    name 	   = "Useful Lua Library",
+    hypocorism = "ULL",
+    version    = 1.0,
+    
+    author = {
+        name 	= "Thomas",
+        country = "Denmark",
+        profile = "http://steamcommunity.com/profiles/76561197999017482/",
+        steamID = "STEAM_0:0:19375877",
+    },
 }
 
 --Instances--
@@ -101,195 +101,195 @@ ULL.INSTANCE.SERVER
 ULL.INSTANCE.CLIENT
 ]]
 function ULL.Include(path, instance)
-	if SERVER then
-		if instance == ULL.INSTANCE.SHARED or instance == ULL.INSTANCE.CLIENT then --If the file should be added to client
-			AddCSLuaFile(path)
-		end
-		
-		if instance == ULL.INSTANCE.SHARED or instance == ULL.INSTANCE.SERVER then --If the file should be ran on server
-			include(path)
-		end
-	end
-	
-	if CLIENT and (instance == ULL.INSTANCE.SHARED or instance == ULL.INSTANCE.CLIENT) then --If the file should be ran on client
-		include(path)
-	end
+    if SERVER then
+        if instance == ULL.INSTANCE.SHARED or instance == ULL.INSTANCE.CLIENT then --If the file should be added to client
+            AddCSLuaFile(path)
+        end
+        
+        if instance == ULL.INSTANCE.SHARED or instance == ULL.INSTANCE.SERVER then --If the file should be ran on server
+            include(path)
+        end
+    end
+    
+    if CLIENT and (instance == ULL.INSTANCE.SHARED or instance == ULL.INSTANCE.CLIENT) then --If the file should be ran on client
+        include(path)
+    end
 end
 
 --Find player by steamID
 function ULL.GetPlayerBySteamID(steamID)
-	for k, v in pairs(player.GetHumans()) do
-		if v:SteamID() == steamID then
-			return v
-		end
-	end
-	
-	return NULL
+    for k, v in pairs(player.GetHumans()) do
+        if v:SteamID() == steamID then
+            return v
+        end
+    end
+    
+    return NULL
 end
 
 --Creates a random color and returns it
 function ULL.RandomColor()
-	return Color(math.random(0, 255), math.random(0, 255), math.random(0, 255))
+    return Color(math.random(0, 255), math.random(0, 255), math.random(0, 255))
 end
 
 --Creates a random color with alpha and returns it
 function ULL.RandomColorAlpha()
-	return Color(math.random(0, 255), math.random(0, 255), math.random(0, 255), math.random(0, 255))
+    return Color(math.random(0, 255), math.random(0, 255), math.random(0, 255), math.random(0, 255))
 end
 
 --The ultimate question of life the universe and everything
 function ULL.TheUltimateQuestionOfLifeTheUniverseAndEverything()
-	return 42
+    return 42
 end
 
 
 if SERVER then
-	
-	----Network strings----
-	util.AddNetworkString("ULL_CLIENT_ChatAddText")
-	
-	
-	----Functions----
-	
-	--[[Returns a object to use for MySQL
-	
-	template:
-	local mySQL = ULL.MySQL{
-		hostname = "",
-		username = "",
-		password = "",
-		database = "",
-		port 	 = 0,
-		
-		OnConnected = function(mySQL)
-			
-		end,
-	}
-	
-	WARNING: This function is unfinished
-	]]
-	function ULL.MySQL(...)
-		require("mysqloo")
-		
-		local mySQL = {}
-		local arg = ...
-		
-		mySQL.quene			  = {}
-		mySQL.firstConnection = true
-		mySQL.OnConnected 	  = arg.OnConnected
-		mySQL.connectionInfo  = {
-			hostname = arg.hostname,
-			username = arg.username,
-			password = arg.password,
-			database = arg.database,
-			port	 = arg.port,
-		}
-		
-		--Connect
-		mySQL.connection = mysqloo.connect(
-			mySQL.connectionInfo.hostname,
-			mySQL.connectionInfo.username,
-			mySQL.connectionInfo.password,
-			mySQL.connectionInfo.database,
-			mySQL.connectionInfo.port
-		)
-		
-		function mySQL.connection:onConnected()
-			ServerLog("Connection to database \""..mySQL.connectionInfo.database.."\" succeed!")
-			
-			if mySQL.firstConnection then
-				mySQL.firstConnection = false
-				
-				--Run OnConnected hook
-				if mySQL.OnConnected then
-					mySQL.OnConnected(mySQL)
-				end
-			end
-			
-			--Run queries in queue
-			for k, v in pairs(mySQL.queue) do
-				mySQL.Query(self, v.sql, v.callback)
-			end
-			
-			--Clear quene
-			mySQL.queue = {}
-		end
-		 
-		function mySQL.connection:onConnectionFailed(err)
-			ServerLog("Connection to database \""..mySQL.connectionInfo.database.."\" failed!\n    [ERROR] "..err)
-		end
-		
-		function mySQL.Query(sql, callback)
-			local query = mySQL.connection:query(sql)
-			
-			--If query failed
-			if !query then
-				--Add query to quene
-				table.insert(mySQL.queue, {
-					sql = sql,
-					callback = callback
-				})
-				
-				--Connect again
-				mySQL.connection:connect()
-				
-				return
-			end
-			
-			if callback then
-				--onSuccess hook
-				function query:onSuccess(data)
-					callback(data)
-				end
-			end
-			
-			function query:onError(err)
-				if mySQL.connection:status() == mysqloo.DATABASE_NOT_CONNECTED then
-					--Add query to quene
-					table.insert(mySQL.queue, {
-						sql = sql,
-						callback = callback
-					})
-					
-					--Connect again
-					mySQL.connection:connect()
-					
-					return
-				end
-				
-				ServerLog("Query on database \""..mySQL.connectionInfo.database.."\" failed!\n    [ERROR] "..err.."\n    [SQL] "..sql)
-			end
+    
+    ----Network strings----
+    util.AddNetworkString("ULL_CLIENT_ChatAddText")
+    
+    
+    ----Functions----
+    
+    --[[Returns a object to use for MySQL
+    
+    template:
+    local mySQL = ULL.MySQL{
+        hostname = "",
+        username = "",
+        password = "",
+        database = "",
+        port 	 = 0,
+        
+        OnConnected = function(mySQL)
+            
+        end,
+    }
+    
+    WARNING: This function is unfinished
+    ]]
+    function ULL.MySQL(...)
+        require("mysqloo")
+        
+        local mySQL = {}
+        local arg = ...
+        
+        mySQL.quene			  = {}
+        mySQL.firstConnection = true
+        mySQL.OnConnected 	  = arg.OnConnected
+        mySQL.connectionInfo  = {
+            hostname = arg.hostname,
+            username = arg.username,
+            password = arg.password,
+            database = arg.database,
+            port	 = arg.port,
+        }
+        
+        --Connect
+        mySQL.connection = mysqloo.connect(
+            mySQL.connectionInfo.hostname,
+            mySQL.connectionInfo.username,
+            mySQL.connectionInfo.password,
+            mySQL.connectionInfo.database,
+            mySQL.connectionInfo.port
+        )
+        
+        function mySQL.connection:onConnected()
+            ServerLog("Connection to database \""..mySQL.connectionInfo.database.."\" succeed!")
+            
+            if mySQL.firstConnection then
+                mySQL.firstConnection = false
+                
+                --Run OnConnected hook
+                if mySQL.OnConnected then
+                    mySQL.OnConnected(mySQL)
+                end
+            end
+            
+            --Run queries in queue
+            for k, v in pairs(mySQL.queue) do
+                mySQL.Query(self, v.sql, v.callback)
+            end
+            
+            --Clear quene
+            mySQL.queue = {}
+        end
+         
+        function mySQL.connection:onConnectionFailed(err)
+            ServerLog("Connection to database \""..mySQL.connectionInfo.database.."\" failed!\n    [ERROR] "..err)
+        end
+        
+        function mySQL.Query(sql, callback)
+            local query = mySQL.connection:query(sql)
+            
+            --If query failed
+            if !query then
+                --Add query to quene
+                table.insert(mySQL.queue, {
+                    sql = sql,
+                    callback = callback
+                })
+                
+                --Connect again
+                mySQL.connection:connect()
+                
+                return
+            end
+            
+            if callback then
+                --onSuccess hook
+                function query:onSuccess(data)
+                    callback(data)
+                end
+            end
+            
+            function query:onError(err)
+                if mySQL.connection:status() == mysqloo.DATABASE_NOT_CONNECTED then
+                    --Add query to quene
+                    table.insert(mySQL.queue, {
+                        sql = sql,
+                        callback = callback
+                    })
+                    
+                    --Connect again
+                    mySQL.connection:connect()
+                    
+                    return
+                end
+                
+                ServerLog("Query on database \""..mySQL.connectionInfo.database.."\" failed!\n    [ERROR] "..err.."\n    [SQL] "..sql)
+            end
 
-			query:start()
-		end
-		
-		mySQL.connection:connect()
-		
-		return mySQL
-	end
-	
-	--chat.AddText() for server
-	/*
-	
-	Examples:
-	ULL.ChatAddText("Send", Entity(1), Color(255, 0, 0), "Hello world!") 				--Send to player 1
-	ULL.ChatAddText("Send", {Entity(1), Entity(2)}, Color(255, 0, 0), "Hello world!") 	--Send to more players
-	ULL.ChatAddText("Broadcast", nil, Color(255, 0, 0), "Hello world!") 				--Send to everyone
-	
-	*/
-	function ULL.ChatAddText(sendType, players, ...)
-		net.Start("ULL_CLIENT_ChatAddText")
-			net.WriteTable(...)
-		net[sendType](players)
-	end
+            query:start()
+        end
+        
+        mySQL.connection:connect()
+        
+        return mySQL
+    end
+    
+    --chat.AddText() for server
+    /*
+    
+    Examples:
+    ULL.ChatAddText("Send", Entity(1), Color(255, 0, 0), "Hello world!") 				--Send to player 1
+    ULL.ChatAddText("Send", {Entity(1), Entity(2)}, Color(255, 0, 0), "Hello world!") 	--Send to more players
+    ULL.ChatAddText("Broadcast", nil, Color(255, 0, 0), "Hello world!") 				--Send to everyone
+    
+    */
+    function ULL.ChatAddText(sendType, players, ...)
+        net.Start("ULL_CLIENT_ChatAddText")
+            net.WriteTable(...)
+        net[sendType](players)
+    end
 end
 
 
 if CLIENT then
-	----Networking----
-	net.Receive("ULL_CLIENT_ChatAddText", function(length)
-		chat.AddText(unpack(net.ReadTable()))
-	end)
+    ----Networking----
+    net.Receive("ULL_CLIENT_ChatAddText", function(length)
+        chat.AddText(unpack(net.ReadTable()))
+    end)
 end
 
 
